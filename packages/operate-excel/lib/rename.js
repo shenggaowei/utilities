241ded56a9649b43bcc8debef71a6f85a31c3c1f
gao <527518190@qq.com>
2021-12-15 16:46:26 +0800

    fix: 脚本读取路径

--- packages/operate-excel/lib/rename.js
+++ packages/operate-excel/lib/rename.js
@@ -1,26 +1,44 @@
-import { getFiles } from "./readFile.js";
 import fs from "fs";
+import glob from "glob";
 import { Command } from "commander";
 import chalk from "chalk";
 
 const program = new Command();
 
-program.option("-t, --type <type>", "add a replace type", "H");
+program.option("-d, --dir <dir>", "the read dir", "./input");
 program.parse();
 
-const { type } = program.opts();
+const { dir } = program.opts();
 
-async function replaceName(colorType) {
-  const pathArray = await getFiles(colorType, "./input");
+async function getFiles() {
+  const data = await new Promise((resolve) => {
+    glob(
+      `**/*.tif`,
+      {
+        cwd: `${dir}`,
+      },
+      function (er, files) {
+        resolve(files);
+      }
+    );
+  });
+  return data;
+}
+
+async function replaceName() {
+  const pathArray = await getFiles();
   pathArray.forEach((pathName) => {
-    const [watcherName, dir, fileName] = pathName.split("/");
+    const ret = pathName.split("/");
+    const rest = ret.slice(0, ret.length - 1);
+    const fileName = ret.slice().pop();
     const reg = /([\s\d]+).([\s\d]+)_([\s\d]+).([\s\d]+)/g;
     const newFileName = fileName.replace(reg, function (str, p1, p2, p3, p4) {
       const newP1 = p1.padStart(p3.length, "0");
       return `${newP1}.${p2}_${p3}.${p4}`;
     });
-    const newPath = `./input/${colorType}/${watcherName}/${dir}/${newFileName}`;
-    fs.renameSync(`./input/${colorType}/${pathName}`, newPath);
+    const currentPaths = rest.join("/");
+    const newPath = `./input/${currentPaths}/${newFileName}`;
+    fs.renameSync(`./input/${pathName}`, newPath);
   });
   console.log(chalk.green("文件更名成功"));
 }
